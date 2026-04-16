90590a63f0c535c238356e83d4f7633b1285db00
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-04-16 10:31:37 +0200

    first possible that could run

--- run.R
+++ run.R
@@ -1,9 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(argparse)
-
-# Source main functions
-source("src/main.R")
+library(HDF5Array)
 
 # Parse command line arguments
 parser <- ArgumentParser(description="OmniBenchmark module")
@@ -13,21 +11,51 @@ parser$add_argument("--output_dir", dest="output_dir", type="character", require
                    help="Output directory for results")
 parser$add_argument("--name", dest="name", type="character", required=TRUE,
                    help="Module name/identifier")
+
 # Stage-specific inputs
-parser$add_argument("--normalized.h5", dest="normalized_h5",
+parser$add_argument("--normalized.h5", dest="input_h5",
                    type="character", nargs="+", required=TRUE,
                    help="Input: normalized.h5")
-parser$add_argument("--filtered.cellids", dest="filtered_cellids",
+parser$add_argument("--filtered.cellids", dest="cellids",
                    type="character", nargs="+", required=TRUE,
                    help="Input: filtered.cellids")
 
+parser$add_argument("--selection_type", dest="selection_type", 
+                    type="character", help="Input file")
+parser$add_argument("--number_selected", dest="number_selected", 
+                    type="character", help="Input file")
+
 args <- parser$parse_args()
 
-cat("Output directory:", args$output_dir, "\n")
-cat("Module name:", args$name, "\n")
-cat("normalized.h5:", args$normalized_h5, "\n")
-cat("filtered.cellids:", args$filtered_cellids, "\n")
+cat("Full command: ", paste0(commandArgs(), collapse = " "), "\n")
+cat("output_dir:", args$output_dir, "\n")
+cat("name:", args$name, "\n")
+cat("selection_type:", args$selection_type, "\n")
+cat("input_h5:", args$input_h5, "\n")
+cat("cellids:", args$cellids, "\n")
 
-# TODO: Implement your module logic
-# Process the data using main function
-process_data(args)
+cellids <- readLines(gzfile(args$cellids))
+cat("length(cellids):", length(cellids), "\n")
+
+m <- TENxMatrix(args$input_h5, group = "matrix")
+m <- as(m, "dgCMatrix") # read into memory
+
+if (args$selection_type == "seurat_vst") {
+  require(Seurat)
+  so <- CreateSeuratObject(counts = m, assay = "normalized")
+  so <- FindVariableFeatures(so, selection.method = "vst", 
+        nfeatures = args$number_selected)
+  sel_feats <- VariableFeatures(so)
+} else if (args$selection_type == "scrapper_modelGeneVariances") {
+  require(SingleCellExperiment)
+  require(scrapper)
+  #sce <- SingleCellExperiment(list(normalized=m)) # not needed, actually
+  gene.var <- modelGeneVariances(m)
+  sel_feats <- chooseHighlyVariableGenes(gene.var$statistics$residuals)
+} else {
+  errorCondition("incorrect 'selection_type' specified")
+}
+
+output_file <- file.path(args$output_dir, paste0(args$name, "_selected.txt.gz"))
+writeLines(sel_feats, gzfile(output_file))
+
